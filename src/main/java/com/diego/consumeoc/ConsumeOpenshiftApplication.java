b62fa16f1168f53cd570211dae269bc321ef3840
diegochavezcarro <diegochavezcarro@gmail.com>
2019-10-10 10:33:48 -0300

    primera subida

---
+++ src/main/java/com/diego/consumeoc/ConsumeOpenshiftApplication.java
@@ -0,0 +1,35 @@
+package com.diego.consumeoc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConsumeOpenshiftApplication {
+	private static final Logger log = LoggerFactory.getLogger(ConsumeOpenshiftApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeOpenshiftApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+
+			ResponseEntity<String> restExchange = restTemplate.exchange(
+					"https://diego-node-secrets-test.apps-np.sis.ad.bia.itau", HttpMethod.GET, null, String.class);
+
+			log.info(restExchange.getBody());
+		};
+	}
+}
---
+++ src/test/java/com/diego/consumeoc/ConsumeOpenshiftApplicationTests.java
@@ -0,0 +1,16 @@
+package com.diego.consumeoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsumeOpenshiftApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
